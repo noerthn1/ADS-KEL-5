644814f69f066520311fbd245382937a96b2a24e
noerthn1 <99663036+noerthn1@users.noreply.github.com>
2023-05-10 22:11:58 +0700

    Update cek.php

--- cek.php
+++ cek.php
@@ -1,36 +1,99 @@
 <?php
-// Connect to the database
+
+// Membuat kelas Connection
+class Connection
+{
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
+    private $conn;
+
+    // Konstruktor kelas Connection, yang akan membuat koneksi ke database
+    public function __construct($servername, $username, $password, $dbname)
+    {
+        $this->servername = $servername;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbname = $dbname;
+
+        // Membuat objek koneksi mysqli
+        $this->conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Jika koneksi gagal, tampilkan pesan error
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    // Metode close() untuk menutup koneksi ke database
+    public function close()
+    {
+        $this->conn->close();
+    }
+
+    // Metode query() untuk menjalankan query SQL pada database
+    public function query($sql)
+    {
+        return $this->conn->query($sql);
+    }
+}
+
+// Membuat kelas Schedule untuk mencari jadwal dokter pada database
+class Schedule
+{
+    private $connection;
+
+    // Konstruktor kelas Schedule
+    public function __construct($connection)
+    {
+        $this->connection = $connection;
+    }
+
+    // Metode find() untuk mencari jadwal dokter berdasarkan tanggal dan sesi
+    public function find($date, $session)
+    {
+        $sql = "SELECT * FROM kuota WHERE tanggal='$date' AND sesi='$session'";
+        return $this->connection->query($sql);
+    }
+}
+
+// Mengisi variabel untuk koneksi ke database
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "haloklinik";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Membuat objek Connection untuk melakukan koneksi ke database
+$connection = new Connection($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-// Check if the form has been submitted
+// Jika tombol submit pada form diklik
 if (isset($_POST['submit'])) {
-    // Get the selected date and session
+    // Mendapatkan tanggal dan sesi yang dipilih
     $date = $_POST['date'];
     $session = $_POST['session'];
 
-    // Fetch jadwal dari database
-    $sql = "SELECT * FROM jadwal WHERE tanggal='$date' AND sesi='$session'";
-    $result = mysqli_query($conn, $sql);
+    // Membuat objek Schedule untuk mencari jadwal dokter
+    $schedule = new Schedule($connection);
 
-    // Menampilkan jadwal di tabel
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row['poli'] . "</td>";
-        echo "<td>" . $row['tanggal'] . "</td>";
-        echo "<td>" . $row['kuota'] . "</td>";
-        echo "</tr>";
+    // Menjalankan query untuk mencari jadwal dokter
+    $result = $schedule->find($date, $session);
+
+    // Jika hasil query mengembalikan baris
+    if ($result->num_rows > 0) {
+        // Tampilkan hasil query pada tabel HTML
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['poli'] . "</td>";
+            echo "<td>" . $row['tanggal'] . "</td>";
+            echo "<td>" . $row['kuota'] . "</td>";
+            echo "</tr>";
+        }
+    } else {
+        // Jika hasil query tidak mengembalikan baris, tampilkan pesan
+        echo "<tr><td colspan='3'>Jadwal Tidak Ditemukan</td></tr>";
     }
 }
 
-// Close the database connection
-mysqli_close($conn);
+// Menutup koneksi ke database
+$connection->close();
